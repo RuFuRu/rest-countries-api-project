4a73cb95726f632ad45f97f492be640d975260e5
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-11-08 19:15:00 +0100

    added mui and used DarkModeOutlinedIcon in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+
 function Header() {
     return (
         <header>
             <h3 className="header-h3">Where in the world?</h3>
-            <button className="header-btn">Dark Mode</button>
+            <button className="header-btn"><DarkModeOutlinedIcon/><span>Dark Mode</span></button>
         </header>
     )
 }
