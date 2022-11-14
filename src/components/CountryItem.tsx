52c9bb13ecc3b148069fcd7176ba73483d64c7ea
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-11-14 19:18:06 +0100

    further styling of CountryList component and it's children

--- src/components/CountryItem.tsx
+++ src/components/CountryItem.tsx
@@ -8,10 +8,12 @@ function CountryItem({countryData}: CountryItemProps) {
     return (
         <div className="country-item-container">
             <div><img src={countryData.flags.svg} alt={`${countryData.cca2} flag`}/></div>
-            <div>{countryData.name.common}</div>
-            <div>{countryData.population}</div>
-            <div>{countryData.region}</div>
-            <div>{countryData.capital[0]}</div>
+            <div className="country-item-description">
+                <h3>{countryData.name.common}</h3>
+                <p><span className="country-item-span">Population:</span>{countryData.population}</p>
+                <p><span className="country-item-span">Region:</span>{countryData.region}</p>
+                <p><span className="country-item-span">Capital:</span>{countryData.capital.join(" ")}</p>
+            </div>
         </div>
     )
 }
