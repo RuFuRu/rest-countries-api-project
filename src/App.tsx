9574bc19b70cbe94718e8a62b8dc8d108ee9ea7e
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-11-16 21:06:51 +0100

    added react-router-dom and routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import Countries from "./components/Countries"
 import Header from "./components/Header"
 import fetchAPI from "./otherFunctions/fetchAPI"
 import { QueryClientProvider, QueryClient } from "react-query"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import CountryItemPage from "./pages/CountryItemPage"
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <QueryClientProvider client={queryClient}>
-        <Countries/>
-      </QueryClientProvider>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<Countries/>}></Route>
+            <Route path="/country-item/:id" element={<CountryItemPage/>}></Route>
+          </Routes>
+        </QueryClientProvider>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/CountryItemPage.tsx
@@ -0,0 +1,7 @@
+function CountryItemPage() {
+    return (
+        <div></div>
+    )
+}
+
+export default CountryItemPage;
