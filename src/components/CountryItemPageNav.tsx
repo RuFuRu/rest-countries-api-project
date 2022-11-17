c4ff4f65f6f942a2b77c5e4d428cead74f012d3c
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-11-17 15:04:30 +0100

    styled button

--- src/components/CountryItemPageNav.tsx
+++ src/components/CountryItemPageNav.tsx
@@ -1,9 +1,15 @@
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import { Link } from 'react-router-dom';
 
 function CountryItemPageNav() {
     return (
-        <div className="country-item-page-nav">
-            <button><KeyboardBackspaceIcon/><span>Back</span></button>
+        <div className="country-item-page-nav-container">
+            <Link to="/">
+                <button>
+                    <KeyboardBackspaceIcon/>
+                    <span>Back</span>
+                </button>
+            </Link>
         </div>
     )
 }
