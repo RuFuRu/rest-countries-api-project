f328f43c6b63af23379b8f076dcc69cac66ccb7a
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-11-26 17:04:21 +0100

    add dark theme version of DarkModeIcon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import { useEffect, useRef } from 'react';
+import { ThemeProvider } from '@emotion/react';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { createTheme } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
 
 interface HeaderProps {
     switchThemes: () => void,
@@ -10,24 +12,43 @@ function Header({switchThemes, theme}: HeaderProps) {
     const headerRef = useRef<HTMLDivElement>(null);
     const h3Ref = useRef<HTMLHeadingElement>(null);
     const spanRef = useRef<HTMLSpanElement>(null);
+    const [iconColour, setIconColour] = useState<any>('primary');
+
+    const iconTheme = createTheme({
+        palette: {
+            primary: {
+                main: "#000000"
+            },
+            secondary: {
+                main: "#ffffff"
+            }
+        }
+    })
 
     useEffect(() => {
         if(theme === "dark") {
             headerRef.current!.style.backgroundColor = "hsl(207, 26%, 17%)";
             h3Ref.current!.style.color = "hsl(0, 0%, 100%)";
             spanRef.current!.style.color = "hsl(0, 0%, 100%)";
+            setIconColour('secondary');
         }
         else if(theme === "light") {
             headerRef.current!.removeAttribute("style");
             h3Ref.current!.removeAttribute("style");
             spanRef.current!.removeAttribute("style");
+            setIconColour('primary');
         }
     },[theme, headerRef, h3Ref, spanRef])
 
     return (
         <header ref={headerRef}>
             <h3 className="header-h3" ref={h3Ref}>Where in the world?</h3>
-            <button className="header-btn" onClick={switchThemes}><DarkModeOutlinedIcon/><span ref={spanRef}>Dark Mode</span></button>
+            <button className="header-btn" onClick={switchThemes}>
+                <ThemeProvider theme={iconTheme}>
+                    <DarkModeIcon color={iconColour}/>
+                </ThemeProvider>
+                <span ref={spanRef}>Dark Mode</span>
+            </button>
         </header>
     )
 }
